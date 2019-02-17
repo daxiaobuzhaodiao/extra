afa2c92c2a5bc1b53d15ef79e68d53f09aca6c2a
vagrant <vagrant@homestead>
2019-02-17 17:22:59 +0000

    address factory and index page

---
+++ app/Models/UserAddress.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class UserAddress extends Model
+{
+    protected $fillable = [
+        'province',
+        'city',
+        'district',
+        'address',
+        'zip',
+        'contact_name',
+        'contact_phone',
+        'last_used_at',
+    ];
+
+    // 声明以后 取出来是个 carbon 时间对象
+    protected $dates = ['last_used_at']; 
+
+    public function user()
+    {
+        return $this->belongsTo('App\Models\User');
+    }
+
+    public function getFullAddressAttribute()
+    {
+        return ($this->province.$this->city.$this->district.$this->address);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -3,3 +3,7 @@
 Auth::routes(['verify' => true]);
 
 Route::get('/', 'HomeController@index')->name('home');
+
+Route::group(['middleware' => ['auth', 'verified']], function () {
+    Route::resource('user_addresses', 'UserAddressesController', ['only' => ['index']]);
+});
